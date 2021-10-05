64a1961ec6763fe41b0f1da12621b49f42f6eec8
Stuart Reid <stuart1334@gmail.com>
2021-10-05 16:54:32 +0100

    Add slice example

---
+++ Chapter3/slices.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := make([]string, 5, 8)
+	slice[0] = "Apple"
+	slice[1] = "Orange"
+	slice[2] = "Banana"
+	slice[3] = "Grape"
+	slice[4] = "Plum"
+
+	inspectSlice(slice)
+
+	slice2 := slice[0:6]
+	slice2[5] = "Test"
+
+	inspectSlice(slice2)
+}
+
+func inspectSlice(slice []string) {
+	fmt.Printf("Length[%d] Capacity[%d]\n", len(slice), cap(slice))
+	for i := range slice {
+		fmt.Printf("[%d] %p %s\n", i, &slice[i], slice[i])
+	}
+}
