3434552c022603885f820b776c5c892d042795ea
Stuart Reid <stuart1334@gmail.com>
2021-10-05 16:39:12 +0100

    Add string memory demonstration

---
+++ Chapter3/memory.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	five := [5]string{"Annie", "Betty", "Charley", "Doug", "Bill"}
+
+	for i, v := range five {
+		fmt.Printf("Value[%s]\tAddress[%p]\tIndexAddr[%p]\n", v, &v, &five[i])
+	}
+}
