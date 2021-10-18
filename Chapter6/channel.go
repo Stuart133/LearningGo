e48a115bbfcd20caf3e4f32ab9ac2e23c5acad0e
Stuart Reid <stuart1334@gmail.com>
2021-10-18 08:52:44 +0100

    Fan out/in pattern

--- Chapter6/channel.go
+++ Chapter6/channel.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	waitForResult()
+	fanOut()
 }
 
 func waitForResult() {
@@ -16,11 +17,35 @@ func waitForResult() {
 	go func() {
 		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 		ch <- "data"
-		fmt.Printf("Child : send signal\n")
+		fmt.Println("Child : send signal")
 	}()
 
 	d := <-ch
 	fmt.Println("parent : recv'd signal :", d)
+
+	time.Sleep(time.Second)
+	fmt.Println("-----------------------------------")
+}
+
+func fanOut() {
+	children := 2000
+	ch := make(chan string, children)
+
+	// Create a goroutine for each unit of work
+	for c := 0; c < children; c++ {
+		go func(child int) {
+			time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
+			ch <- "data"
+			fmt.Println("child : sent signal :", child)
+		}(c)
+	}
+
+	for children > 0 {
+		d := <-ch
+		children--
+		fmt.Println(d)
+		fmt.Println("parent : recv'd signal :", children)
+	}
 
 	time.Sleep(time.Second)
 	fmt.Println("-----------------------------------")
