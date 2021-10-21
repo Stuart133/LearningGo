de7e88c60c39533ade4c596ea5d1323e343c3bcd
Stuart Reid <stuart1334@gmail.com>
2021-10-21 21:34:15 +0100

    Show b.N growth

--- Chapter8/benchmark_test.go
+++ Chapter8/benchmark_test.go
@@ -6,12 +6,20 @@ import (
 )
 
 var gs string
+var a []int
 
 func BenchmarkSprint(b *testing.B) {
 	var s string
+
+	a = append(a, b.N)
 	for i := 0; i < b.N; i++ {
 		s = fmt.Sprint("hello")
 	}
+
+	if len(a) > 4 {
+		fmt.Println(a)
+	}
+
 	gs = s
 }
 func BenchmarkSprintf(b *testing.B) {
