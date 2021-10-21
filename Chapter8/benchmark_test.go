cf1ed47d92fed5a47b8d23854bc15b05914e0783
Stuart Reid <stuart1334@gmail.com>
2021-10-21 21:38:18 +0100

    Show sub benchmarks

--- Chapter8/benchmark_test.go
+++ Chapter8/benchmark_test.go
@@ -9,6 +9,11 @@ var gs string
 var a []int
 
 func BenchmarkSprint(b *testing.B) {
+	b.Run("None", benchmarkSprint)
+	b.Run("Format", benchmarkSprintf)
+}
+
+func benchmarkSprint(b *testing.B) {
 	var s string
 
 	a = append(a, b.N)
@@ -17,12 +22,13 @@ func BenchmarkSprint(b *testing.B) {
 	}
 
 	if len(a) > 4 {
-		fmt.Println(a)
+		// fmt.Println(a)
 	}
 
 	gs = s
 }
-func BenchmarkSprintf(b *testing.B) {
+
+func benchmarkSprintf(b *testing.B) {
 	var s string
 	for i := 0; i < b.N; i++ {
 		s = fmt.Sprintf("hello")
